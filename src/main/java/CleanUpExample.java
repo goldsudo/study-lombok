5f5ce05a5bb784b8cb71df24e2d194dd6ca1c17f
goldsudo <goldsudo@gmail.com>
2018-09-23 23:31:34 +0800

    lombok的clean以及getter&setter注解的exmaple

---
+++ src/main/java/CleanUpExample.java
@@ -0,0 +1,45 @@
+import lombok.Cleanup;
+
+import java.io.*;
+
+public class CleanUpExample {
+
+    public static void old(String[] args) throws IOException{
+        InputStream in = new FileInputStream(args[0]);
+        try {
+            OutputStream out = new FileOutputStream(args[1]);
+            try {
+                byte[] b = new byte[10000];
+                while (true) {
+                    int r = in.read(b);
+                    if (r == -1) break;
+                    out.write(b, 0, r);
+                }
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public static void now(String[] args) throws IOException{
+        @Cleanup InputStream in = new FileInputStream(args[0]);
+        @Cleanup OutputStream out = new FileOutputStream(args[1]);
+        byte[] b = new byte[10000];
+        while (true) {
+            int r = in.read(b);
+            if (r == -1) break;
+            out.write(b, 0, r);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        old(args);
+        now(args);
+    }
+}  
---
+++ src/main/java/GetterSetterExample.java
@@ -0,0 +1,36 @@
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+    public class GetterSetterExample {
+    private int age = 10;
+        private String name;
+
+        @Override public String toString() {
+            return String.format("%s (age: %d)", name, age);
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+        protected void setName(String name) {
+            this.name = name;
+        }
+    }
+*/
+
+public class GetterSetterExample {
+    @Getter @Setter private int age = 10;
+
+    @Setter(AccessLevel.PROTECTED) private String name;
+
+    @Override public String toString() {
+        return String.format("%s (age: %d)", name, age);
+    }
+}
+
